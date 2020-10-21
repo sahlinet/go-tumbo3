2d62932f4e572356e223fed9c3570f36e1936e5a
Philip Sahli <philip@sahli.net>
2020-10-21 19:29:16 +0200

    update runner

--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -1,7 +1,11 @@
 package runner
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	"log"
+	"os"
 	"os/exec"
 	"plugin"
 )
@@ -19,12 +23,27 @@ type SimpleRunnable struct {
 type Execute func() string
 
 func (r SimpleRunnable) Build() error {
-	cmd := exec.Command("/usr/local/bin/go", "build", "-buildmode=plugin")
-	cmd.Dir = r.Location
-	err := cmd.Run()
+	args := []string{"build", "-buildmode=plugin", "github.com/sahlinet/go-tumbo3-examples/helloworld"}
+	//args = []string{"env"}
+	cmd := exec.Command("/usr/local/bin/go", args...)
+	cmd.Dir = "/Users/philipsahli/workspace/go-tumbo3-examples/helloworld"
+	cmd.Env = []string{"GOOS=darwin", "GOARCH=amd64", "GOCACHE=/tmp/a", "GOPATH=/Users/philipsahli/go", "CC=clang", "PATH=/usr/bin"}
+
+	var b bytes.Buffer
+	cmd.Stdout = &b
+	cmd.Stderr = &b
+
+	//stdout, err := cmd.StdoutPipe()
+	//stderr, err := cmd.StderrPipe()
+	err := cmd.Start()
 	if err != nil {
 		return fmt.Errorf("build error: %s", err)
 	}
+
+	if err := cmd.Wait(); err != nil {
+		log.Print(err)
+	}
+	println("output: " + string(b.Bytes()))
 
 	return nil
 }
@@ -33,7 +52,12 @@ func (r SimpleRunnable) Run() (string, error) {
 	var p *plugin.Plugin
 	var err error
 
-	p, err = plugin.Open(fmt.Sprintf("%s/helloworld.so", r.Location))
+	//p, err = plugin.Open(fmt.Sprintf("%s/helloworld.so", r.Location))
+	if _, err := os.Stat("./helloworld.so"); os.IsNotExist(err) {
+		// Do whatever is required on module not existing
+		return "", errors.New("file not found")
+	}
+	p, err = plugin.Open(fmt.Sprint("./helloworld.so"))
 	if err != nil {
 		return "", err
 	}
