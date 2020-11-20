a72d71b7fc1cd98feb009b57129b7be72f9e92af
Philip Sahli <philip@sahli.net>
2020-11-20 08:43:27 +0100

    remove elm-old

--- web/elm-old/src/Asset.elm
+++
@@ -1,9 +0,0 @@
-module Asset exposing (Image, defaultAvatar, error, loading, src)
-
-{-| Assets, such as images, videos, and audio. (We only have images for now.)
-We should never expose asset URLs directly; this module should be in charge of
-all of them. One source of truth!
--}
-
-import Html exposing (Attribute, Html)
-import Html.Attributes as Attr
