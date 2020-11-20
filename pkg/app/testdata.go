65276b924f2cb485b0057b1b413b1cdf6f1672a1
Philip Sahli <philip@sahli.net>
2020-11-20 08:14:09 +0100

    use dynamic path

--- pkg/app/testdata.go
+++ pkg/app/testdata.go
@@ -2,10 +2,18 @@ package app
 
 import (
 	"log"
+	"os"
+	"path"
+	"path/filepath"
 
 	"gorm.io/gorm"
 
 	"github.com/sahlinet/go-tumbo3/pkg/models"
+)
+
+var (
+	dir, _ = os.Getwd()
+	fp     = filepath.Join(filepath.Dir(dir), "..")
 )
 
 func TestData(db *gorm.DB) {
@@ -15,12 +23,14 @@ func TestData(db *gorm.DB) {
 		Description: "a project to test",
 		State:       0,
 		GitRepository: &models.GitRepository{
-			Url: "/Users/philipsahli/workspace/go-tumbo3/examples/example-plugin-go-grpc",
+			Url: path.Join(fp, "examples/example-plugin-go-grpc"),
 		},
 		Services: []models.Service{{
 			Name: "service-A",
 		}},
 	}
+
+	log.Print(project)
 
 	err := db.Model(&project).Association("GitRepository").Error
 	if err != nil {
